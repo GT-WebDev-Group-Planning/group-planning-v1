a3ec65bc4c1b583306dd06f92ff040b28ae55041
Amy <45153739+amtang325@users.noreply.github.com>
2023-09-09 14:33:34 -0400

    init commit

---
+++ server/db/connect.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+  return mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+}
+
+module.exports = connectDB

ca89d982345557cbdbbcf7479b73c7bdb8779e90
dbaglodi <baglodidisha@gmail.com>
2023-09-26 19:09:58 -0400

    Adding buttons and headers
    Added some buttons and headers and also a canvas to create the invitation

--- client/src/pages/Evite.js
+++ client/src/pages/Evite.js
@@ -2,6 +2,13 @@ function Evite() {
     return (
       <div className="Evite">
         <h1>Evite Page</h1>
+        <h2>Attendees</h2>
+        <h2>Invitation</h2>
+        <canvas id = "invitation" width = "100" height = "50"></canvas>
+        <button type = "button">Color</button>
+        <button type = "button">Images</button>
+        <button type = "button">Text</button>
+        <button type = "submit">Send Invitation</button>
       </div>
     );
   }

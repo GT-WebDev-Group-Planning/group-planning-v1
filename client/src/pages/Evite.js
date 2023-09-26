a79f8da6dd5034b05733ded6917bbb746eb8a746
dbaglodi <baglodidisha@gmail.com>
2023-09-26 19:37:11 -0400

    more styling stuff
    Barbie Font (possibly, if it works)

--- client/src/pages/Evite.js
+++ client/src/pages/Evite.js
@@ -1,14 +1,14 @@
 function Evite() {
     return (
       <div className="Evite">
-        <h1>Evite Page</h1>
+        <h1 color="#f4a1e9" style = "font: Lucida Handwriting">Evite Page</h1>
         <h2>Attendees</h2>
         <h2>Invitation</h2>
-        <canvas id = "invitation" width = "100" height = "50"></canvas>
-        <button type = "button">Color</button>
-        <button type = "button">Images</button>
-        <button type = "button">Text</button>
-        <button type = "submit">Send Invitation</button>
+        <canvas id = "invitation" width = "150" height = "100"></canvas>
+        <button type = "button" color = "#d6300d">Color</button>
+        <button type = "button" color = "#2a85ec">Images</button>
+        <button type = "button" color = "#000000">Text</button>
+        <button type = "submit" color = "#08f525">Send Invitation</button>
       </div>
     );
   }

a44097e2ecb7654ff4d8bd645649ece80e8aa2db
Amy <45153739+amtang325@users.noreply.github.com>
2023-10-03 16:28:17 -0400

    added oauth for accessing calendar permissions

--- server/app.js
+++ server/app.js
@@ -3,12 +3,36 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const { google } = require('googleapis');
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+)
+
+const scopes = [
+  'https://www.googleapis.com/auth/calendar'
+];
+
 //connectDB
 const connectDB = require('./db/connect');
 
 
 app.get('/', (req, res) => {
   res.send('<h1>Testing</h1>');
+})
+
+app.get('/google', (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes
+  })
+
+  res.redirect(url);
+});
+
+app.get('/google/redirect', (req, res) => {
+  res.send("redirect works!");
 })
 
 const port = process.env.PORT || 5000;

1c64e293147e4c1320de2dff504efa1907a36900
Blake <KennethSweezy@gmail.com>
2023-10-03 18:37:44 -0400

    Add Calendar Select Page
    TODO:
    Center the damn button

---
+++ client/src/pages/CalendarSelect/CalendarSelect.js
@@ -0,0 +1,12 @@
+import React from "react"
+import CalendarSelectWindow from "./CalendarSelectWindow"
+
+function CalendarSelect() {
+    return (
+        <div className="CalendarSelect">
+            <CalendarSelectWindow></CalendarSelectWindow>
+        </div>
+    );
+}
+
+export default CalendarSelect;

b91bf26b9c36995325f22f221e1f283e7c22622f
gtrotsuka <otsukaryan@gmail.com>
2023-10-03 18:45:44 -0400

    login-page layout update

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,8 +1,23 @@
+import React from "react";
+
 function Login() {
   return (
     <div className="LogIn">
       <h1>Log In Page</h1>
+        <p style={{textAlign: "center"}}>
+        Username: 
+        </p>
+          <input type="text" placeholder="Username"/>
+        <p style={{textAlign: "center"}}>
+        Password:
+        </p>
+	        <input type="text" placeholder="Password"/>
+
+          <button variant="text">Login</button>
     </div>
+
+    
+    
   );
 }
 

bca8d66809b93f2dd3c467b557a67c4252514ccd
dbaglodi <baglodidisha@gmail.com>
2023-10-03 19:28:44 -0400

    Update Evite.js

--- client/src/pages/Evite.js
+++ client/src/pages/Evite.js
@@ -3,12 +3,15 @@ function Evite() {
       <div className="Evite">
         <h1 color="#f4a1e9">Evite Page</h1>
         <h2>Attendees</h2>
-        <h2>Invitation</h2>
+        <h2 align="left">Invitation</h2>
         <canvas id = "invitation" width = "150" height = "100"></canvas>
-        <button type = "button" color = "#d6300d">Color</button>
+        <button color = "#d6300d">Color</button>
         <button type = "button" color = "#2a85ec">Images</button>
         <button type = "button" color = "#000000">Text</button>
-        <button type = "submit" color = "#08f525">Send Invitation</button>
+        <br />
+        <button type = "submit" color="#08f525">Send Invitation</button>
+        <br />
+        <rect width="150" height="100" />
       </div>
     );
   }

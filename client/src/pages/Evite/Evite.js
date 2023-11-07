1ab013f509654568f8b60fba62414ffc28777b71
dbaglodi <baglodidisha@gmail.com>
2023-11-07 18:53:10 -0500

    design changes for create event

--- client/src/pages/Evite/Evite.js
+++ client/src/pages/Evite/Evite.js
@@ -2,23 +2,34 @@ import React, { useState } from 'react';
 import invitation from './invitation_picture.png';
 import friends from './friends.png'
 import { Link } from "react-router-dom";
+import './evite.css'
 function Evite() {
     function Dropdown(){
         const [option, setOption] = useState('');
         const handleChange = (e) => {
         setOption(e.target.value);
+        };
         return (
           <div>
             <select value={option} onChange={handleChange}>
             <option value="">Select an option</option>
-            <option value="Group 1">Option 1</option>
-            <option value="Group 2">Option 2</option>
-            <option value="Group 3">Option 3</option>
+            <option value="Group 1">Group 1</option>
+            <option value="Group 2">Group 2</option>
+            <option value="Group 3">Group 3</option>
             </select>
-            <p>{option}</p>
           </div>
           );
-        };
+    }
+    function DisplayBox(props) {
+      return (
+        <div>
+        <ul>
+        {props.items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+        </div>
+      );
     }
     return (
       <div className="Evite">
@@ -26,14 +37,18 @@ function Evite() {
         <br /><br /><br />
         <h2>Group</h2>
         <Dropdown></Dropdown>
+        <h2>Title</h2>
+        <input type="text"></input>
         <h2>Description</h2>
         <input type="text"></input>
-        <p>Date{"\t"}{"\t"}{"\t"}{"\t"}Time</p>
-        <button type = "submit" color="Red">Send Invitation</button>
+        <p>Date</p><p>Time</p>
+        <button type = "submit">Send Invitation</button>
         <h2>Invitees</h2></center>
         <img src={invitation} alt="Invitation" width="300" height="250" />
+        <center><DisplayBox title="Invitees" items={['friend1', 'friend2', 'friend3']}></DisplayBox></center>
         <img src={friends} alt="Mike and Sully" width="260" height="330" align="right" />
         <br />
+        <button type="button" align="Bottom">Add Invitee</button>
         <button type="button" align="Bottom"><Link to="/calendar">Back</Link></button>
         <button type="button" align="Bottom"><Link to="/">Home</Link></button>
       </div>

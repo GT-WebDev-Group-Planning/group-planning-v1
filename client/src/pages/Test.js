955e5cb8e3214c1c439caffbd79fb1eaf833a8b9
Amy <45153739+amtang325@users.noreply.github.com>
2023-11-14 19:13:17 -0500

    finished login page

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import './test.css';
 
 function Test() {
   return (
-    <div>
-      <a href="http://localhost:5000/google">Log into Google</a>
+    <div className='gray-div'>
+      <h1>WELCOME!</h1>
+      <p>Sign in to continue to Group Planning App.</p>
+      <button><a href="http://localhost:5000/google">Sign in with Google</a></button>
     </div>
   );
 }

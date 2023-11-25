9570f0fcae548d59b34e96e24d80c60932b0b809
Amy <45153739+amtang325@users.noreply.github.com>
2023-11-25 18:11:18 -0500

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Group Planning</h1>
         <button><Link to="/calendar">Calendar</Link></button>
-        <button><Link to="/evite">Create Event</Link></button>
+        {/* <button><Link to="/evite">Create Event</Link></button> */}
         <button><Link to="/group">Group</Link></button>
         <button><Link to="/joingroup">Join Group</Link></button>
         {/* <button><Link to="/login">Login</Link></button> */}
